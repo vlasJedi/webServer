86b57117ed833d9a0096d49d95a9a759814e08d8
vlas <vlas.d78@gmail.com>
2019-06-02 22:35:40 +0300

    deleted-unpropriate-files

---
+++ src/main/java/com/webApp/addPack/Demo.java
@@ -0,0 +1,11 @@
+package com.webApp.addPack;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.webApp.mainPack.Console;
+public class Demo {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-init-bean.xml");
+        Console console = (Console) context.getBean("console");
+        console.logMessage();
+    }
+}
